025352b32b6ad6bf6787d415fa120a82509d4d3b
Jared Perez <JTPerez@live.com>
2022-06-09 11:27:13 -0700

    Inital

---
+++ FirstJavaProgram/FJP.java
@@ -0,0 +1,11 @@
+public class FJP {
+    public static void main(String[] args) {
+        String name = "Billy Bob";
+        String age = "32";
+        String homeTown = "Walnut Creek, CA";
+
+        System.out.println("My Name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + homeTown);
+    }
+}
